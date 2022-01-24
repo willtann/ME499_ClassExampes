281d713f2e43812ecff1fcf225bcf3a62c61a208
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-24 09:44:20 -0800

    Lecture 8

---
+++ recursionL8.py
@@ -0,0 +1,16 @@
+# /users/tannerwilliams/desktop/ME 499/ME499_ClassExamples
+# Recursion defined by a function that calls itself
+
+def median_of_two_lists(A, B):
+    """
+    :param A: sorted list 'A'
+    :param B: sorted list 'B'
+    :return: median of each list
+    """
+    if len(A) > 1:
+
+
+
+
+
+def merge_sort(A):
